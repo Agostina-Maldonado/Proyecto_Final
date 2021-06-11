b5aa561efbcdeb30ea260a2e337debb2aa32165b
matias-maldonado <mmaldonadoaraya@udesa.edu.ar>
2021-06-11 12:18:12 -0300

    cambios

--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 //HEADER
 //buscador
-<<<<<<< HEAD
-let formulario = document.querySelector("form"); 
+/*let formulario = document.querySelector("form"); 
 let campoBuscar = document.querySelector(".Buscar"); 
 let alert = document.querySelector(".alert"); 
 formulario.addEventListener('submit', function(event) {
@@ -13,14 +12,8 @@ alert.innerText="El campo tiene que tener al menos 3 caracteres para poder mostr
 }else{
 this.submit();
 }
-})
-=======
-let queryString= location.search;
-let queryStringToObject= new URLSearchParams(queryString);
-let buscar= queryStringToObject.get{"buscar"};
-let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${buscar}`;
-let datoBuscado: document.querySelector (".datoBuscado");
-datoBuscado.innerText = buscar
+})*/
+
 
 //carrousel
 var slideIndex = 1;
@@ -52,14 +45,15 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-let url= 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0';
+let url= 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks';
 fetch (url)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
+        console.log(data);
         let sencillos= document.querySelector('.sencillos');
-        let infoCanciones= data.tracks.data;
+        let infoCanciones= data.data;
         let contenidoSencillos= ''
         for (let i=0;i<infoCanciones.length;i++){
             contenidoSencillos += `<article class="info">
@@ -82,7 +76,7 @@ fetch(artistas)
         let infoArtistas= data.data;
         let contenidoArtistas= ''
         for (let i=0;i<infoArtistas.length;i++){
-            contenidoSencillos += `<article class="info">
+            contenidoArtistas += `<article class="info">
             <img src= "${infoArtistas[i].picture_medium}">
             <h2><a href="detalle_artistas.html?id=${infoArtistas[i].id}">${infoArtistas[i].name}</a></h2></article>`
         } artistasDestacados.innerHTML += contenidoArtistas;
@@ -102,8 +96,8 @@ fetch(albumes)
         let infoAlbumes= data.data;
         let contenidoAlbumes= ''
         for (let i=0;i<infoAlbumes.length;i++){
-            contenidoSencillos += `<article class="info">
-            <img src= "${infoArtistas[i].cover_medium}">
+            contenidoAlbumes += `<article class="info">
+            <img src= "${infoAlbumes[i].cover_medium}">
             <h2><a href="detalle_disco.html?id=${infoAlbumes[i].id}">${infoAlbumes[i].tittle}</a></h2>
             <p><a href="detalle_artistas.html?id=${infoAlbumes[i].id}">${infoAlbumes[i].artist.name}</a></p>
             </article>`
@@ -112,5 +106,4 @@ fetch(albumes)
     .catch(function(error){
         console.log(error);
     })
->>>>>>> b8986fe70ea50c205314b7ba75f409b4b23182cf
 
