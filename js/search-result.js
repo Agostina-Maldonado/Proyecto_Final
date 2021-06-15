cf6be18e720b41c9956bd814b8bbc8d57bdf9c06
matias-maldonado <mmaldonadoaraya@udesa.edu.ar>
2021-06-15 13:29:51 -0300

    search mas cambios

--- js/search-result.js
+++ js/search-result.js
@@ -1,6 +1,6 @@
 let queryString = new URLSearchParams(location.search)
-let Buscar= queryString.get ('barra-buscador');
-artistas = ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=' + buscar)
+let buscar= queryString.get ('buscar');
+let artistas = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q='+buscar
 fetch(artistas)
     .then(function(response){
         return response.json();
@@ -9,8 +9,8 @@ fetch(artistas)
         console.log(data);
         let listArtist= document.querySelector(".lista_artistas");
         let infoArtistas= data.data ;
-        let contenido = ''
-        for( let i=0; i<infoArtistas.lenght;i++){
+        let contenido = '';
+        for( let i=0; i<infoArtistas.length;i++){
             contenido += `	<article class="imagen_y_texto">
             <div class="card-image">
                 <img class="img_artista" src="${infoArtistas[i].picture_small}" alt="justin bieber">
@@ -25,7 +25,7 @@ fetch(artistas)
     .catch(function(error){
         console.log(error);
         })
- albumes = ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q=' + buscar)
+let albumes = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q='+buscar
 fetch(albumes)
     .then(function(response){
         return response.json();
@@ -35,10 +35,10 @@ fetch(albumes)
         let listAlbum= document.querySelector(".lista_artistas");
         let infoAlbum= data.data ;
         let contenidoAlbum = ''
-        for( let i=0; i<infoAlbum.lenght;i++){
+        for( let i=0; i<infoAlbum.length;i++){
             contenidoAlbum += `<article class="imagen_y_texto">
                 <div class="card-image">
-                    <img class="img_artista" src="${infoalbum[i].cover_small}" alt="justin bieber">
+                    <img class="img_artista" src="${infoAlbum[i].cover_small}" alt="justin bieber">
                  </div>
                 <div class="card-nombre_artista">
                     <h3><a href="./detalle_disco.html?id=${infoAlbum[i].id}">${infoAlbum[i].title}</a></h3>
@@ -52,3 +52,27 @@ fetch(albumes)
         console.log(error);
         })
 
+let Canciones = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q='+buscar
+fetch(Canciones)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+        let songs= document.querySelector(".informacionResultados");
+        let infoCanciones= data.data ;
+        let contenidoCanciones = '';
+        for( let i=0; i<infoCanciones.length;i++){
+            contenidoCanciones +=	
+            `<td class="player"><img src="${infoCanciones[i].artist.picture_small}"></td>
+            <td><a href="detalle_cancion.html?id=${infoCanciones[i].id}">${infoCanciones[i].title}</a></td>
+            <td></td>
+            <td><a href="detalle_artistas.html?id=${infoCanciones[i].id}">${infoCanciones[i].artist.name}</a></td>
+            <td><a href="detalle_disco.html?id=${infoCanciones[i].id}">${infoCanciones[i].album.title}</a></td>
+            <td>${infoCanciones[i].duration}</td>`
+        }
+        songs.innerHTML += contenidoCanciones;
+        })
+    .catch(function(error){
+        console.log(error);
+        })
