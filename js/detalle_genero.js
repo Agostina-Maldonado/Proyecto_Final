f4ef9bae4a17e363f09012ec4c923ec4f2e2f248
Your Name <you@example.com>
2021-06-18 12:18:11 -0300

    buscadores

--- js/detalle_genero.js
+++ js/detalle_genero.js
@@ -1,4 +1,26 @@
 window.addEventListener('load', function(){
+    //HEADER
+//buscador
+let formulario = document.querySelector("form"); 
+let campoBuscar = document.querySelector(".barra-buscador"); 
+let alert = document.querySelector(".alertTotal"); 
+formulario.addEventListener('submit', function(event){
+event.preventDefault(); //Detenemos el comportamiento default del formulario que es enviarse.
+    if (campoBuscar.value==''){ //Chequeamos el contenido.
+        alert.innerHTML= `<p class="alert">El campo está vacío, escribe al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
+    }else if (campoBuscar.value.lenght<3){
+        alert.innerHTML= `<p class="alert">El campo tiene que tener al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
+    }else{
+        this.submit()
+        iconError.style.display= "none"
+    }   
+
+    //limpiar el mensaje de error cuando el usario modifique el contenido del campo input.
+ campoBuscar.addEventListener('input', function(){
+    alert.innerHTML ='';
+    })
+})
+
 let queryString = location.search //Caputramos la qs
 let queryStringToObject = new URLSearchParams(queryString); //La transformamos en OL
 let id = queryStringToObject.get('id');
