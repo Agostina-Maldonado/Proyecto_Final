ade349ece26cf63f314a3008c128034222d1ce83
matias-maldonado <mmaldonadoaraya@udesa.edu.ar>
2021-06-22 10:00:04 -0300

    playlist

--- js/playlist.js
+++ js/playlist.js
@@ -19,3 +19,29 @@ event.preventDefault(); //Detenemos el comportamiento default del formulario que
     alert.innerHTML ='';
     })
 })
+let idTrack= //(completar con el id de la cancion que quiero agregar en playlist)//
+let url= 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q='+idTrack
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+        let songs= document.querySelector(".informacionResultados");
+        let infoCanciones= data.data ;
+        let contenidoCanciones = '';
+        for( let i=0; i<infoCanciones.length;i++){
+            contenidoCanciones +=	
+            `<tr> 
+                <td class="player><img src="${infoCanciones[i].artist.picture_small}"><a href="detalle_cancion.html?id=${infoCanciones[i].id}">${infoCanciones[i].title}</a></td>
+                <td></td>
+                <td><a href="detalle_artistas.html?id=${infoCanciones[i].id}">${infoCanciones[i].artist.name}</a></td>
+                <td><a href="detalle_disco.html?id=${infoCanciones[i].id}">${infoCanciones[i].album.title}</a></td>
+                <td>${infoCanciones[i].duration}</td>
+            </tr>`
+        }
+        songs.innerHTML += contenidoCanciones;
+        })
+    .catch(function(error){
+        console.log(error);
+        })
