b266b6225535653d606866c105a1dc632073fb21
Your Name <you@example.com>
2021-06-24 01:02:23 -0300

    proligidad header y algunas correcciones

--- js/detalle_artistas.js
+++ js/detalle_artistas.js
@@ -5,13 +5,13 @@ let campoBuscar = document.querySelector(".barra-buscador");
 let alert = document.querySelector(".alertTotal"); 
 formulario.addEventListener('submit', function(event){
 event.preventDefault(); //Detenemos el comportamiento default del formulario que es enviarse.
-    if (campoBuscar.value==''){ //Chequeamos el contenido.
+    if (campoBuscar.value==''){ 
         alert.innerHTML= `<p class="alert">El campo está vacío, escribe al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
     }else if (campoBuscar.value.length<3){
         alert.innerHTML= `<p class="alert">El campo tiene que tener al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
     }else{
-        this.submit()
-        iconError.style.display= "none"
+        this.submit();
+        alert.style.display="none";
     }   
 
     //limpiar el mensaje de error cuando el usario modifique el contenido del campo input.
@@ -19,7 +19,8 @@ event.preventDefault(); //Detenemos el comportamiento default del formulario que
     alert.innerHTML ='';
     })
 })
-console.log('hola');
+
+//DETALLE ARTISTAS
 let queryString = location.search;
 let queryStringToObject = new URLSearchParams(queryString);
 let id = queryStringToObject.get('id');
--- js/search-result.js
+++ js/search-result.js
@@ -6,13 +6,13 @@ let campoBuscar = document.querySelector(".barra-buscador");
 let alert = document.querySelector(".alertTotal"); 
 formulario.addEventListener('submit', function(event){
 event.preventDefault(); //Detenemos el comportamiento default del formulario que es enviarse.
-    if (campoBuscar.value==''){ //Chequeamos el contenido.
+    if (campoBuscar.value==''){ 
         alert.innerHTML= `<p class="alert">El campo está vacío, escribe al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
     }else if (campoBuscar.value.length<3){
         alert.innerHTML= `<p class="alert">El campo tiene que tener al menos 3 caracteres para poder mostrarte resultados</p><i class="far fa-times-circle"></i>`;
     }else{
-        this.submit()
-        iconError.style.display= "none"
+        this.submit();
+        alert.style.display="none";
     }   
 
     //limpiar el mensaje de error cuando el usario modifique el contenido del campo input.
